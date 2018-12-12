3171f106eafd0fd1c2c0d4a27d14508f915e1cfa
rubenperacuni <rubenperacuni@usal.es>
2018-12-12 14:25:34 +0100

    a

--- vistas/piezas/menu_lateral.php
+++ vistas/piezas/menu_lateral.php
@@ -7,12 +7,7 @@
 
 		<ul>
 			<li><a href="?a=inicio">Home</a></li>
-			<li><a href="?a=accuweather">Meteorología</a></li>
-			<li><a href="?a=trafico">Tráfico</a></li>
-			<li><a href="?a=aire">Calidad del aire</a></li>
-			<li><a href="?a=agenda">Agenda Cultural</a></li>
 			<li><a href="?a=enunciado">Enunciado</a></li>
-			<li><a href="?a=empleo">Empleo</a></li>
 			<li><a href="?a=elementos">Elementos web</a></li>
 		</ul>
 	</nav>
