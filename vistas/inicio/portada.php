486508f53f4dbb08c8e951502250aaa74f0448f6
nurimateos6 <nurimateos@usal.es>
2018-12-29 18:56:44 +0100

    Update portada.php

--- vistas/inicio/portada.php
+++ vistas/inicio/portada.php
@@ -1,57 +1,10 @@
+
 <div id="main">
 	<div class="inner">
-		<h1>Tu test</h1>
+		<h1>BIENVENIDO A TU TEST</h1>
 		<section>
-			<h2>Portada de la página</h2>
-			<p>Ejemplo de código en terminal:</p>
+			<h2>Tu plataforma para realizar test e impulsar tu aprendizaje</h2>
+			<p>Con los mejores profesores</p>
+			<?php $imagen="inicio"; echo '<img src="images/'.$imagen.'.jpg" />';?>
 			<pre><code class="console">
-promt$ ls
-Carpeta1    Carpeta2    Carpeta3
-promt$
-			</code></pre>
-		</section>
-    <section>
-      <h2>Portada de la página</h2>
-      <p>Ejemplo de código java:</p>
-	     <pre><code class="javascript">
-function $initHighlight(block, cls) {
-  try {
-    if (cls.search(/\bno\-highlight\b/) != -1)
-      return process(block, true, 0x0F) +
-             ` class="${cls}"`;
-  } catch (e) {
-    /* handle exception */
-  }
-  for (var i = 0 / 2; i < classes.length; i++) {
-    if (checkCondition(classes[i]) === undefined)
-      console.log('undefined');
-  }
-}
-
-export  $initHighlight;
-		
-}       </code></pre>
-
-      <h2>Portada de la página</h2>
-      <p>Ejemplo de código json:</p>
-<pre><code class="json" >{
-  "name": "John",
-  "age": 30,
-  "cars": [{
-      "name": "Ford",
-      "models": ["Fiesta", "Focus", "Mustang"]
-    },
-    {
-      "name": "BMW",
-      "models": ["320", "X3", "X5"]
-    },
-    {
-      "name": "Fiat",
-      "models": ["500", "Panda"]
-    }
-  ]
-}
-</code></pre>
-	</div>
-</div>
 
