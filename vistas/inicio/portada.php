1a6b468b4e74b5e4f086000097b9ae6f2fd6b850
nurimateos6 <nurimateos@usal.es>
2018-12-29 20:03:15 +0100

    Update portada.php

--- vistas/inicio/portada.php
+++ vistas/inicio/portada.php
@@ -1,14 +1,30 @@
+	<div id="ini">
+ 	<div style="display: inline-block;">
+ 	<?php vista::generarPieza( 'boton_accion', array( 'texto'=>'Iniciar Sesión',
+    'activo'=>true, 'url'=>array('a'=>'inicio.login'), 
+    'submit'=>true)); 
+    ?>
+</div>
+<div>
+    <?php
+ 	vista::generarPieza( 'boton_accion', array( 'texto'=>'Registrarse',
+    'activo'=>true, 'url'=>array('a'=>'inicio.registro'), 
+    'submit'=>true));?>
+</div><br/></div>
+
 
 <div id="main">
+
 	<div class="inner">
+ 
 		<h1>BIENVENIDO A TU TEST</h1>
+
 		<section>
 			<h2>Tu plataforma para realizar test e impulsar tu aprendizaje</h2>
 			<p>Con los mejores profesores</p>
 			<?php $imagen="inicio"; echo '<img src="images/'.$imagen.'.jpg" />';?>
-			<pre><code class="console">
 
-
+			<br/><br/>
 	</div>
 </div>
 
