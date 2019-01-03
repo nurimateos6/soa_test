ad5541c2a5b8c4f696ab1fa0b28572f396119e9c
nurimateos6 <nurimateos@usal.es>
2019-01-03 18:01:10 +0100

    Create profesor.php

---
+++ vistas/profesor/profesor.php
@@ -0,0 +1,15 @@
+<div id="main">
+
+	<div class="inner">
+ 
+		<h1>ADMINISTRACIÓN DEL PROFESOR</h1>
+
+		<ol>
+			<li><a href="?a=alumno">Administración de alumnos</li>
+			<li><a href="?a=pregunta">Administración de test</li>
+			<li><a href="?a=resultado">Ver resultados de los alumnos</li>
+	
+		</ol>
+	</div>
+</div>
+
